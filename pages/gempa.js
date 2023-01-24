688bb8cd98293be3b10406b02b19b1299520d7d3
Ari Pradhana <aripradhana20@gmail.com>
2023-01-24 09:05:34 +0700

    Add Routes Gempa

---
+++ pages/gempa.js
@@ -0,0 +1,15 @@
+import WeatherEarthquakeSource from "../data/cuacaindb-source.js";
+
+const Gempa = {
+    async render() {
+        return `
+            <div class="gempa-container"></div>
+        `;
+    },
+
+    async afterRender() {
+        
+    }
+}
+
+export default Gempa;
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
+import Gempa from "../pages/gempa.js";
 import Home from "../pages/home.js";
 
 const routes = {
     '/': Home,
     '/home': Home,
+    '/gempa': Gempa,
 }
 
 export default routes;
